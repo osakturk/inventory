0ffe7136dbc70936342901579e3df5f285767a99
Omer Akturk <omer.akturk@etpa.nl>
2023-03-05 00:58:40 +0100

    refactor: add comments for logics

--- src/main/java/com/product/inventory/service/ArticleService.java
+++ src/main/java/com/product/inventory/service/ArticleService.java
@@ -18,18 +18,22 @@ public class ArticleService {
         this.articleRepository = articleRepository;
     }
 
-
+    //We can create articles using this method
     public String create(ArticleRequest articleRequest){
         articleRepository.insert(articleRequest.getInventory());
         return Constants.ARTICLE_CREATION_MESSAGE;
     }
 
+    //We can get article list using this method
     public List<Article> getList(){
         return articleRepository.findAll();
     }
+
+    //We reduce stocks using this method. This method is triggered by the order creation API
     public void decreaseStock(String articleId, Long decreaseAmount){
         Optional<Article> article = articleRepository.findById(articleId);
         article.ifPresent(presentedArticle -> {
+            //If the article doesn't have enough material, it should throw a meaningful exception
            if (decreaseAmount > presentedArticle.getStock()){
                throw new EnoughMaterialNotFoundException(Constants.ENOUGH_MATERIAL_NOT_FOUND_MESSAGE);
            }
